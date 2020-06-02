1c664b050505e337ec53dde060af917cf8cb847c
daan23411 <46263539+daan23411@users.noreply.github.com>
2020-06-02 20:45:30 +0200

    Update ticketClose.js

--- cmds/ticketClose.js
+++ cmds/ticketClose.js
@@ -16,6 +16,7 @@ module.exports.run = async (client, message, args) => {
 
     var embedCreateTicket = new discord.MessageEmbed()
         .setTitle("Ticket " + message.channel.name)
+        .setColor("AQUA")
         .setDescription("Het ticket is gemarkeerd als **compleet**. Als je nog vragen hebt of niet tevreden bent twijfel dan niet en maak nog een ticket aan!")
         .setFooter("© daan2341, 2020 - 2021");
 
