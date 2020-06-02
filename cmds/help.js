02f67f6fa8621d97fb5c905491b752240c16f7a0
daan23411 <46263539+daan23411@users.noreply.github.com>
2020-06-02 16:17:09 +0200

    Update help.js

--- cmds/help.js
+++ cmds/help.js
@@ -4,7 +4,7 @@ module.exports.run = async(client, message, args) => {
 
     try{
 
-        var text = "**Help Menu** \n\n **__COMMANDS:__** \n\n **GENERAL** \n !hallo - Groet terug! \n !serverinfo - Geeft server informatie. \n\n **MODERATOR** \n !kick - Kick een speler. (!kick (mention) (reden)) \n !ban - Verban een speler. (!ban (mention) (reden) \n !warn - Warn een speler (!warn (mention) (reden)) \n !tempmute - Tijdelijk muten van een speler (!tempmute) (speler) (tijd) \n\n **ADMIN** \n !clear - Verwijderd een aantal berichten (!clear (aantal)) \n\n **MEER CMDS SOON**"
+        var text = "**Help Menu** \n\n **__COMMANDS:__** \n\n **GENERAL** \n !hallo - Groet terug! \n !serverinfo - Geeft server informatie. \n\n **MODERATOR** \n !kick - Kick een speler. (!kick (mention) (reden)) \n !ban - Verban een speler. (!ban (mention) (reden) \n !warn - Warn een speler (!warn (mention) (reden)) \n !tempmute - Tijdelijk muten van een speler (!tempmute) (speler) (tijd) \n\n **ADMIN** \n !clear - Verwijderd een aantal berichten (!clear (aantal)) \n !announce - Maak een mededeling! (!announcement titel | bericht | kleur (HEX Code) | kanaal (geen #)) \n\n **MEER CMDS SOON**"
 
         message.author.send(text);
 
