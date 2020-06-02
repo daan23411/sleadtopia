8c695dcac9c7d05c18d4c8d3c57ba91b02f4ceb8
daan23411 <46263539+daan23411@users.noreply.github.com>
2020-06-02 20:35:10 +0200

    Create ticketClose.js

---
+++ cmds/ticketClose.js
@@ -0,0 +1,35 @@
+const discord = require("discord.js");
+
+module.exports.run = async(client, message, args) => {
+
+    const categoryID = "713337970828115969";
+
+    if(!message.member.hasPermission("KICK_MEMBER")) return message.reply("Jij kan dit niet! Vraag een admin om je ticket te closen!");
+
+    if(message.channel.parentID == categoryID){
+        message.channel.delete();
+    }else {
+
+        message.channel.send("Doe dit in een ticket kanaal!");
+
+    }
+
+    var embedCreateTicket = new discord.MessageEmbed()
+    .setTitle("Ticket, " + message.channel.name)
+    .setDescription("Het ticket is gemarkeerd als **compleet**. Als je nog vragen hebt of niet tevreden bent twijfel dan niet en maak nog een ticket aan!")
+    .setFooter(" **TICKET COMPLEET**,© daan2341, 2020 - 2021");
+
+    var ticketChannel = message.member.guild.channels.cache.find(channel => channel.name === "logs");
+    if(!ticketChannel) return message.reply("Kanaal bestaat niet!");
+
+    var ticketChannelDM = channel.type("dm");
+
+    ticketChannelDM.send(embedCreateTicket);
+
+    ticketChannel.send(embedCreateTicket);
+
+}
+
+module.exports.help = {
+    name: "close"
+}
