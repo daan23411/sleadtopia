39052177b8eb5d4d7803b264c8e3ebbdaf920fcf
daan23411 <46263539+daan23411@users.noreply.github.com>
2020-06-02 20:37:51 +0200

    Update ticketClose.js

--- cmds/ticketClose.js
+++ cmds/ticketClose.js
@@ -1,28 +1,29 @@
 const discord = require("discord.js");
 
-module.exports.run = async(client, message, args) => {
+module.exports.run = async (client, message, args) => {
 
     const categoryID = "713337970828115969";
 
-    if(!message.member.hasPermission("KICK_MEMBER")) return message.reply("Jij kan dit niet! Vraag een admin om je ticket te closen!");
+    if (!message.member.hasPermission("KICK_MEMBER")) return message.reply("Jij kan dit niet! Vraag een admin om je ticket te closen!");
 
-    if(message.channel.parentID == categoryID){
+    if (message.channel.parentID == categoryID) {
         message.channel.delete();
-    }else {
+    } else {
 
         message.channel.send("Doe dit in een ticket kanaal!");
 
     }
 
     var embedCreateTicket = new discord.MessageEmbed()
-    .setTitle("Ticket, " + message.channel.name)
-    .setDescription("Het ticket is gemarkeerd als **compleet**. Als je nog vragen hebt of niet tevreden bent twijfel dan niet en maak nog een ticket aan!")
-    .setFooter(" **TICKET COMPLEET**,© daan2341, 2020 - 2021");
+        .setTitle("Ticket, " + message.channel.name)
+        .setDescription("Het ticket is gemarkeerd als **compleet**. Als je nog vragen hebt of niet tevreden bent twijfel dan niet en maak nog een ticket aan!")
+        .setFooter(" **TICKET COMPLEET**,© daan2341, 2020 - 2021");
 
     var ticketChannel = message.member.guild.channels.cache.find(channel => channel.name === "logs");
-    if(!ticketChannel) return message.reply("Kanaal bestaat niet!");
+    if (!ticketChannel) return message.reply("Kanaal bestaat niet!");
 
-    var ticketChannelDM = channel.type("dm");
+    if (message.author.bot) return;
+    if (message.channel.type === "dm") return;
 
     ticketChannelDM.send(embedCreateTicket);
 
